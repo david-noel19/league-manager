a6c2c53bc97eebaaf40f79a89f03747d9b948ebb
David Noel <dnoel@ecg-hq.com>
2022-04-21 10:30:06 -0600

    updating db connection and db tables

--- src/database/migrations/1611089925984-init-db.ts
+++ src/database/migrations/1611089925984-init-db.ts
@@ -2,33 +2,22 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class initDb1611089925984 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    // await queryRunner.query(
-    //     "CREATE USER apiuser WITH PASSWORD '';"
-    // );
+
     await queryRunner.query('GRANT connect ON DATABASE leagues TO apiuser;');
-    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS member;');
+    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS leaguemanager;');
     await queryRunner.query('CREATE SCHEMA IF NOT EXISTS person;');
-    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS team;');
-    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS match;');
-    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS staff;');
-    await queryRunner.query('CREATE SCHEMA IF NOT EXISTS audit;');
     await queryRunner.query(
-      'ALTER ROLE apiuser SET search_path TO public, league, audit;',
+      'ALTER ROLE apiuser SET search_path TO public, league;',
     );
     await queryRunner.query(
-      'GRANT USAGE ON SCHEMA public, league, audit TO apiuser;',
+      'GRANT USAGE ON SCHEMA public, leaguemanager TO apiuser;',
     );
     await queryRunner.query(
-      'alter default privileges in schema public, league, audit grant all on tables to apiuser;',
-    );
-    await queryRunner.query(
-      'alter default privileges in schema public, league, audit grant all on sequences to apiuser;',
+      'alter default privileges in schema public, leaguemanager grant all on tables to apiuser;',
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    // await queryRunner.query('DROP SCHEMA IF EXISTS acm CASCADE;');
-    // await queryRunner.query("DROP EXTENSION \"uuid-ossp\" WITH SCHEMA acm;");
-    await queryRunner.query('DROP SCHEMA IF EXISTS audit CASCADE;');
+    await queryRunner.query('DROP SCHEMA IF EXISTS leaguemanager CASCADE;');
   }
 }
